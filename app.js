aa3c60e87b470afb70c149a0c88e3d64581298fb
KennySinners <odeukuracsve@gmail.com>
2019-07-25 05:45:09 +0200

    Replaced several functions by extending existing ones

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ const {
     AllChannelsWithTimeout,
     AllNSFWChannels
 } = require("./collections");
-const { Guild } = require('discord.js');
+const {
+    Guild,
+    Collection
+} = require('discord.js');
 
 class Info extends Guild {
 
@@ -19,9 +22,10 @@ class Info extends Guild {
     }
 
 
-    getGuild(id) {
+    getGuild(id, asmsg = Boolean) {
         /**
-         * @param {String} id 
+         * @param {String} id
+         * @param {Boolean} asmsg
          * @type {Collection}
          */
 
@@ -33,14 +37,20 @@ class Info extends Guild {
             throw new Error("Client was not able to find the specified guild ID")
         } else {
 
-            guildCollection.set(this.guild.id, this.guild.name);
-            return guildCollection;
+            guildCollection.set(this.guild.id, this.guild);
+
+            if (!asmsg || asmsg === false) {
+                return guildCollection.map(gld => gld)
+            } else {
+                return guildCollection.map(gl => gl.name + " | Member Count: " + gl.memberCount)
+            }
         }
     }
 
-    getOwner(id) {
+    getOwner(id, asmsg = Boolean) {
         /**
          * @param {String} id
+         * @param {Boolean} asmsg
          * @type {Collection}
          */
         this.guild = this.client.guilds.get(id);
@@ -51,50 +61,21 @@ class Info extends Guild {
             throw new Error("Client was not able to find the specified guild ID")
         } else {
 
-            ownerCollection.set(this.guild.owner.id, this.guild.owner.user.tag);
-            return ownerCollection;
+            ownerCollection.set(this.guild.owner.id, this.guild.owner);
+
+            if (!asmsg || asmsg === false) {
+                return ownerCollection;
+            } else { 
+                return ownerCollection.map(x => x.user.username + " | " + x.user.id)
+            }
         }
     }
 
-    getGuildInfo(id) {
+    getChannelsByType(id, type, asmsg = Boolean) {
         /**
          * @param {String} id
-         * @type {Collection}
-         */
-        this.guild = this.client.guilds.get(id);
-
-        if (!id) {
-            throw new Error("Please provide an ID parameter")
-        } else if (!this.client.guilds.get(id)) {
-            throw new Error("Client was not able to find the specified guild ID")
-        } else {
-
-            detailedGuildCollection.set(this.guild.id, this.guild)
-            return detailedGuildCollection;
-        }
-    }
-
-    getOwnerInfo(id) {
-        /**
-         * @param {String} id
-         * @type {Collection}
-         */
-        this.guild = this.client.guilds.get(id);
-
-        if (!id) {
-            throw new Error("Please provide an ID parameter")
-        } else if (!this.client.guilds.get(id)) {
-            throw new Error("Client was not able to find the specified guild ID")
-        } else {
-
-            detailedOwnerCollection.set(this.guild.owner.id, this.guild.owner)
-            return detailedOwnerCollection;
-        }
-    }
-
-    getChannelsByType(id, type) {
-        /**
-         * @param {String} id
+         * @param {String} type
+         * @param {Boolean} asmsg(true || false)
          * @type {Collection}
          */
         this.guild = this.client.guilds.get(id);
@@ -109,8 +90,14 @@ class Info extends Guild {
             throw new Error("Client was not able to find the specified guild id")
         } else {
 
-            ChannelsCollectionByType.set(this.guild.id, this.guild.channels.filter(ch => ch.type.toLowerCase() === type).map(s => s.name + " | " + s.id))
-            return ChannelsCollectionByType;
+            ChannelsCollectionByType.set(this.guild.id, this.guild.channels.filter(ch => ch.type === type))
+
+
+            if (!asmsg || asmsg === false) {
+                return ChannelsCollectionByType.map(s => s)
+            } else {
+                return ChannelsCollectionByType.map(c => c.name + " | " + c.id);
+            }
         }
     }
 
@@ -127,12 +114,14 @@ class Info extends Guild {
             throw new Error("Client was not able to find the specified guild ID")
         } else {
 
-            AllChannelsByObjCollection.set(this.guild.id, this.guild.channels.forEach(ch => ch))
+            AllChannelsByObjCollection.set(this.guild.id, this.guild.channels.map(ch => ch))
+
             return AllChannelsByObjCollection;
+
         }
     }
 
-    getAllChannelsByName(id) {
+    getAllChannelsByName(id, asmsg) {
         /**
          * @param {String} id
          * @type {Collection}
@@ -146,16 +135,26 @@ class Info extends Guild {
         } else {
 
             AllChannelsByName.set(this.guild.id, this.guild.channels.map(c => c.name))
-            return AllChannelsByName;
+
+            if (!asmsg) {
+                return AllChannelsByName;
+            } else {
+                if (AllChannelsByID.array().length > 50) {
+                    throw new Error("Cannot display 50 values. Please consider console logging the collection")
+                }
+
+                return AllChannelsByName.map(cl => cl.join("\n"));
+            }
         }
     }
 
-    getAllChannelsByID(id) {
+    getAllChannelsByID(id, asmsg) {
         /**
          * @param {String} id
          * @type {Collection}
          */
         this.guild = this.client.guilds.get(id);
+
 
         if (!id) {
             throw new Error("Please provide an ID parameter")
@@ -164,13 +163,23 @@ class Info extends Guild {
         } else {
 
             AllChannelsByID.set(this.guild.id, this.guild.channels.map(ch => ch.id))
-            return AllChannelsByID;
+
+            if (!asmsg) {
+                return AllChannelsByID;
+            } else {
+                if (AllChannelsByID.array().length > 50) {
+                    throw new Error("Cannot display 50 values. Please consider console logging the collection")
+                } else {
+                    return AllChannelsByID.map(c => c.join("\n"))
+                }
+            }
         }
     }
 
-    getAllChannelsWithTimeout(id) {
+    getAllChannelsWithTimeout(id, asmsg) {
         /**
          * @param {String} id
+         * @param {Boolean} asmsg
          * @type {Collection}
          */
         this.guild = this.client.guilds.get(id);
@@ -180,15 +189,24 @@ class Info extends Guild {
         } else if (!this.client.guilds.get(id)) {
             throw new Error("Client was not able to find the specified guild ID")
         } else if (!this.guild.channels.find(c => c.rateLimitPerUser > 0)) {
-            return "Couldn't find any guild with a timeout."
+            return "Couldn't find any channel with a timeout."
         } else {
 
-            AllChannelsWithTimeout.set(this.guild.id, this.guild.channels.map(c => c.type === "text" || c.type === "news" && c.rateLimitPerUser > 0))
-            return AllChannelsWithTimeout;
+            AllChannelsWithTimeout.set(this.guild.id, this.guild.channels.filter(c => (c.type === "text" || c.type === "news") && c.rateLimitPerUser > 0).map(ch => ch.name))
+
+            if (!asmsg) {
+                return AllChannelsWithTimeout;
+            } else {
+                if (AllChannelsByID.array().length > 50) {
+                    throw new Error("Cannot display 50 values. Please consider console logging the collection")
+                }
+
+                return AllChannelsWithTimeout.map(cl => cl.join("\n"))
+            }
         }
     }
 
-    getAllNSFWChannels(id) {
+    getAllNSFWChannels(id, asmsg) {
         /**
          * @param {String} id
          * @type {Collection}
@@ -203,8 +221,20 @@ class Info extends Guild {
             return "Couldn't find any NSFW channels."
         } else {
 
-            AllNSFWChannels.set(this.guild.id, this.guild.channels.filter(ch => ch.nsfw === true).map(s => s))
-            return AllNSFWChannels;
+            AllNSFWChannels.set(this.guild.id, this.guild.channels.filter(ch => ch.nsfw))
+
+            if (!asmsg) {
+
+                return AllNSFWChannels;
+
+            } else if (asmsg === true) {
+                if (AllChannelsByID.array().length > 50) {
+                    throw new Error("Cannot display 50 values. Please consider console logging the collection")
+                }
+
+                return AllNSFWChannels.map(col => col.map(ch => ch.name))
+
+            }
         }
     }
 }
